ca643c56ce435e6276fd1cf381ce340ebaa350f0
blaine-t <blaine-t@bathost.net>
2025-08-29 22:34:57 -0500

    Parsing calendar PoC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::read_to_string;
+
+use icalendar::{Calendar, CalendarComponent, Component};
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_to_string("test.ics").unwrap();
+
+    let parsed_calendar: Calendar = contents.parse().unwrap();
+
+    for component in &parsed_calendar.components {
+        if let CalendarComponent::Event(event) = component {
+            println!("Event: {}", event.get_summary().unwrap())
+        }
+    }
 }
