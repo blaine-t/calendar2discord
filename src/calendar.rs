ebc7d9d7f890bed3e8296a075a5d1fffa422ee22
blaine-t <blaine-t@bathost.net>
2025-08-30 01:41:06 -0500

    Add basic multicalendar support

--- src/calendar.rs
+++ src/calendar.rs
@@ -4,23 +4,31 @@ use std::fs::read_to_string;
 use crate::util::date_perhaps_time_to_utc;
 
 pub fn get_current_event() -> Option<Event> {
-    let contents = read_to_string("test.ics").unwrap();
+    let calendars = std::fs::read_dir("calendars")
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.path().extension().and_then(|s| s.to_str()) == Some("ics"))
+        .collect::<Vec<_>>();
 
-    let parsed_calendar: Calendar = contents.parse().unwrap();
+    for calendar in calendars {
+        let contents = read_to_string(calendar.path()).unwrap();
 
-    let now = chrono::Utc::now();
+        let parsed_calendar: Calendar = contents.parse().unwrap();
 
-    for component in &parsed_calendar.components {
-        if let CalendarComponent::Event(event) = component {
-            let start_time = date_perhaps_time_to_utc(&event.get_start().unwrap());
-            let end_time = date_perhaps_time_to_utc(&event.get_end().unwrap());
-            if start_time < now && end_time > now {
-                println!(
-                    "Event: {}. Starts at: {:?}",
-                    event.get_summary().unwrap(),
-                    start_time
-                );
-                return Some(event.clone());
+        let now = chrono::Utc::now();
+
+        for component in &parsed_calendar.components {
+            if let CalendarComponent::Event(event) = component {
+                let start_time = date_perhaps_time_to_utc(&event.get_start().unwrap());
+                let end_time = date_perhaps_time_to_utc(&event.get_end().unwrap());
+                if start_time < now && end_time > now {
+                    println!(
+                        "Event: {}. Starts at: {:?}",
+                        event.get_summary().unwrap(),
+                        start_time
+                    );
+                    return Some(event.clone());
+                }
             }
         }
     }
